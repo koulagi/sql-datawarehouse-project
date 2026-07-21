663a11572fd40a0e2153f9940c02a61d5f5d0ce6
koulagi <supriyaskoulagi2000@gmail.com>
2026-07-21 14:16:43 +0530

    Update advance_analytics.sql

--- scripts/advance_analytics.sql
+++ scripts/advance_analytics.sql
@@ -1,4 +1,6 @@
---Advance Analytics
+--=============================================================================
+-- Advanced Analytics Queries
+--=============================================================================
 
 --1. Change-over-time
 --aggregate[Measure] By [DateDimension]
@@ -83,3 +85,84 @@
 	END year_change
 	FROM yearly_product_sales
 	ORDER BY product_name,order_year
+
+--4.Part to whole analysis
+--[Measure]/Total[Measure])*100 By [dimension]
+		
+	--Which categories contribute the most to overall sales
+	WITH category_sales AS
+	(SELECT 
+	category,
+	SUM(sales_amount) AS total_sales
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+	GROUP BY category)
+	
+	SELECT
+	category,
+	total_sales,
+	SUM(total_sales) OVER() AS overall_sales,
+	CONCAT(ROUND(100* CAST(total_sales AS FLOAT)/SUM(total_sales) OVER(),2),'%')  AS percentage_of_total
+	FROM category_sales
+	ORDER BY total_sales DESC
+
+--5.Data Segmentation
+--Group the data based on specific range
+--[Measure] By [Measure]
+	
+	/*Segment products into cost ranges and
+	count how many products fall into each segment*/
+
+	WITH segment AS 
+	(
+	SELECT
+	product_key,
+	product_name,
+	cost,
+	CASE WHEN cost <100 THEN 'Below 100'
+		 WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+		 WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+		 ELSE 'Above 1000'
+	END cost_range
+	FROM gold.dim_products)
+
+	SELECT
+	cost_range,
+	COUNT(product_key) AS total_products
+	FROM segment
+	GROUP BY cost_range
+	ORDER BY total_products DESC
+
+	/*Group customers into three segments on their spending behaviour:
+	-VIP at least 12 months of history and spending more than 5000.
+	-Regular: at least 12 months of history but spending 5000 or less
+	- New: lifespan less than 12 months.*/
+
+	WITH customer_spending
+	AS (SELECT
+	c.customer_key,
+	SUM(f.sales_amount) AS total_spending,
+	MIN(order_date) AS first_order,
+	MAX(order_date) AS last_order,
+	DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key
+	GROUP BY c.customer_key)
+
+	SELECT
+	customer_segment,
+	COUNT(customer_key) AS total_customers
+	FROM (SELECT
+	customer_key,
+	CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+		 WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+		 ELSE 'New'
+	END customer_segment
+	FROM customer_spending)t
+	GROUP BY customer_segment
+	ORDER BY total_customers DESC
+
+
+
